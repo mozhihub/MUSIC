08f82312497644d51d6406f7955f849ea7e3eb1a
KAVIYARASAN <comradekaviyarasan@gmail.com>
2024-12-24 17:30:36 +0530

    Update script.js

--- assist/js/script.js
+++ assist/js/script.js
@@ -40,7 +40,7 @@ const musicData = [
     album: "tamil hits",
     year:2000.2022,
     artist: "Ar.Rahuman",
-    musicPath: "https://firebasestorage.googleapis.com/v0/b/storage-b08d5.appspot.com/o/arrahuman.webm?alt=media&token=b6b3a0fc-d621-4e9e-ac9b-9f23164ef3bf",
+    musicPath: "https://firebasestorage.googleapis.com/v0/b/storage-b08d5.appspot.com/o/arrahuman.mp3?alt=media&token=95027ac0-1613-4e53-8fd6-e99de4987aba",
   },
   {
     backgroundImage: "./assist/images/poster-4.jpg",
