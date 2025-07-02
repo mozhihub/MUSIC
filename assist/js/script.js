90e835ca397a581d79749a725fed9648d4175b7e
KAVIYARASAN <comradekaviyarasan@gmail.com>
2025-07-02 08:10:15 +0530

    Update script.js

--- assist/js/script.js
+++ assist/js/script.js
@@ -455,6 +455,15 @@ const shuffle = function () {
 playerShuffleBtn.addEventListener("click", shuffle);
 
 
+function shuffleArray(array) {
+  const shuffled = array.slice(); // copy array
+  for (let i = shuffled.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
+  }
+  return shuffled;
+}
+
 
 /**
  * REPEAT MUSIC
